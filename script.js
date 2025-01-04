52516cd54f06a839c7b8e4e6f4744d8dfe33a3d3
Atalay <atalayozmen21@gmail.com>
2025-01-04 22:38:02 +0100

    add visitor counter

---
+++ script.js
@@ -0,0 +1,9 @@
+// Purpose: To fetch the visitor count from the AWS Lambda function and display it on the website.
+
+const counter = document.querySelector(".visitor-count-banner");
+async function getVisitorCount() {
+    const response = await fetch("https://uqgjrhe5kt4bogme4l26gna6iq0nheeq.lambda-url.eu-west-1.on.aws/");
+    const data = await response.json();
+    counter.innerHTML = 'You are visitor number ' + data + '!';
+}
+getVisitorCount();
