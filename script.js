fcbdabbc9be3fad65383d046894008438096ef99
Atalay <atalayozmen21@gmail.com>
2025-01-04 22:47:09 +0100

    add small message for counter

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ const counter = document.querySelector(".visitor-count-banner");
 async function getVisitorCount() {
     const response = await fetch("https://uqgjrhe5kt4bogme4l26gna6iq0nheeq.lambda-url.eu-west-1.on.aws/");
     const data = await response.json();
-    counter.innerHTML = 'You are visitor number ' + data + '!';
+    counter.innerHTML = 'You are visitor number ' + data + '! \n This counter is powered by DynamoDB and AWS Lambda.';
 }
 getVisitorCount();
